d4f0aa2a0b6fed1dc905bedc55f7a0962e4e61d9
ajitdevv <ajitdev014@gmail.com>
2026-02-21 16:18:17 +0530

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
 import Link from "next/link"
 export default async function Home() {
-const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/blogs`, {
-  cache: "no-store",
-})
+  const baseUrl =
+    process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : "http://localhost:3000"
+
+  const res = await fetch(`${baseUrl}/api/blogs`, {
+    cache: "no-store",
+  })
   const blogs = await res.json()
   return (
     <div className="min-h-screen bg-background">
